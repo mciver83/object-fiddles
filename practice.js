92005a6e7bb0756162fe1d9b6b4e2c7dddf2d737
Mark McIver <markmciver83@gmail.com>
2015-05-25 13:03:40 -0600

    answers to object practice problems

--- practice.js
+++ practice.js
@@ -3,7 +3,12 @@
 
 //Create an object called me. Give it a key of name with the value being your name, and another key of age with the value being your age. Then alert your name using dot notation.
 
-  //Code here
+  var Me = {
+    name: 'Mark',
+    age: 32
+  };
+
+  alert(Me.name);
 
 
 
@@ -13,19 +18,30 @@
 
 
 
-//Make a 'favoriteThings' object that contains the following keys: band, food, person, book, movie, holiday. Have the values to those keys be your favorite thing in that category.
+//Make a 'favoriteThings' object that contains the following keys: band, food, person, book, movie, holiday. 
+//Have the values to those keys be your favorite thing in that category.
 
-  //Code here
+  var favoriteThings = {
+    band: "matchbox 20",
+    food: "mexican",
+    person: "Carolyn",
+    book: "Harry Potter",
+    movie: "Divergent",
+    holiday: "Christmas"
+  }
+
 
 
 //After you've made your object, add another key named 'car' with the value being your favorite car and then another key named 'brand' with the value being your favorite brand.
 
-  //Code here
+  favoriteThings.car = "Tesla";
+  favoriteThings.brand = "Apple";
 
 
 //Now change the food key in your favoriteThings object to be 'Lettuce' and change the book key in your favoriteThings object to be '50 Shades of Gray'.
 
-  //Code here
+  favoriteThings.food = "Lettuce";
+  favoriteThings.book = "50 Shades of Gray";
 
 
 
@@ -42,17 +58,23 @@ Set the value of that key to 'chapstick'.
 Using dot notation, add another key (or property) to your backPack object
 that is named color, with the value being the color of your backpack. */
 
-  //Code here
+  var backPack = {};
+
+  var item = "firstPocket";
+
+  backPack[item] = "chapstick";
+
+  backPack.color = "Blue";
 
 //After you do the above, alert your entire backPack object.
 
-  //Code here
+  alert(backPack);
 
 /*You probably noticed that it just alerted [object Object].
 Alerting to see the data in your Object doesn't work so well.
 Instead, console.log your whole backPack object and then check out the console. */
 
-  //Code here
+  console.log(backPack);
 
 
 
@@ -64,11 +86,21 @@ Instead, console.log your whole backPack object and then check out the console. 
 
 //Create another 'me' object with the following properties name, age, height, gender, married, eyeColor, hairColor. Fill those properties in with the appropriate values.
 
-  //Code Here
+  var me = {
+    name: 'Mark',
+    age: 32,
+    height: "6'1",
+    gender: 'M',
+    married: false,
+    eyeColor: 'Blue',
+    hairColor: 'Brown'
+  }
 
 //Now, loop through your object and alert every value. *Tyler --> 24 --> 6'0 --> Male, etc etc
 
-  //Code Here
+  for(var key in me){
+    alert(me[key]);
+  }
 
 
 
@@ -80,11 +112,19 @@ Instead, console.log your whole backPack object and then check out the console. 
 
 //Create an Object called 'album' with 5 keys named different song titles that you make up, with the values being the length of each song.
 
-  //Code Here
+  var album = {
+    loveSong: '5:32',
+    chillSong: '3:23',
+    danceSong: '6:04',
+    rockSong: '3:46',
+    otherSong: '0:32'
+  }
 
 //Now, loop through your album object alerting every song title individually.
 
-  //Code Here
+  for(var key in album){
+    alert(key);
+  }
 
 
 
@@ -96,11 +136,21 @@ Instead, console.log your whole backPack object and then check out the console. 
 
 //Create an object called states that has 5 US states as properties with the values being their population (doesn't have to be accurate).
 
-  //Code Here
+  var states = {
+    hawaii: 1342965,
+    utah: 245,
+    california: 34569843,
+    colorado: 2345212,
+    iadho: 234532
+  }
 
 //Now, loop through your states object and if the states population is greater than 30K, alert that state.
 
-  //Code Here
+  for(var key in states){
+    if(states[key] > 30000){
+      alert(key);
+    }
+  }
 
 
 
@@ -121,11 +171,17 @@ var user = {
 /*Above you're given a user object. Loop through the user object checking to make sure
 that each value is truthy. If it's not truthy, remove it from the object. */
 
-  //Code Here
+  for(var key in user){
+    if(!user[key]){
+      delete user[key];
+    }
+  }
 
 //Once you get your truthy Object, Change the remaining values in the object to be specific to you (name: 'your name', username: 'your username'), rather than my information.
 
-  //Code Here
+  user.name = 'Mark';
+  user.pwHash = 'sdfwerwdsfhldf';
+  user.username = 'mciver83';
 
 
 
@@ -149,11 +205,14 @@ var user = {
 //Let's say I, the user, decided to change my name and email address to the following
 // name -> 'Tyler S. McGinnis', email -> 'tyler.mcginnis@devmounta.in'. Make that change.
 
-  //Code Here
+  
+  user.name = 'Tyler S. McGinnis';
+  user.email = 'tyler.mcginnis@devmounta.in';
+
 
 //Now call the sayName method that's on the user object which will alert the users email
 
-  //Code Here
+  user.sayName();
 
 
 
@@ -165,17 +224,25 @@ var user = {
 
 //Create an empty object called methodCollection.
 
-  //Code Here
+  var methodCollection = {};
 
 /*Now add two methods (functions that are properties on objects) to your methodCollection
 object. One called 'alertHello' which alerts 'hello' and another method called logHello
  which logs 'hello' to the console. */
 
-  //Code Here
+  methodCollection.alertHello = function(){
+    alert('hello');
+  }
+
+  methodCollection.logHello = function(){
+    console.log('hello');
+  }
 
 //Now call your alertHello and logHello methods.
 
-  //Code Here
+  methodCollection.alertHello();
+
+  methodCollection.logHello();
 
 
 
@@ -215,12 +282,18 @@ var colt = {
 array with those four objects. After that console.log the length of the Array and make
 sure that it's equal to 4. */
 
-  //Code Here
+  devMountainEmployees.push(tyler, cahlan, ryan, colt);
+
+  console.log(devMountainEmployees.length);
 
 /*Now let's say Cahlan has a mental breakdown and has to take a leave of absense to 'find himself'.
 Loop through your devMountainEmployees until you find cahlan, then remove him from the array.*/
 
-  //Code Here
+  for(var i = 0; i < devMountainEmployees.length; i++){
+    if(devMountainEmployees[i].name === 'Cahlan'){
+      delete devMountainEmployees[i];
+    }
+  }
 
 
 
